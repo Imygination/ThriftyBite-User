f4e0bf80bcc033d60b927ca94047eeb85d431d51
Gema <gemaramadhan20@gmail.com>
2023-11-15 08:33:58 +0700

    Main FUnction Navigators Done

---
+++ mobile-user/src/navigator/MainStack.js
@@ -0,0 +1,22 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import DetailScreen from "../screens/DetailScreen";
+import LoginScreen from "../screens/LoginScreen";
+import SignUpScreen from "../screens/SignUpScreen";
+import MessageScreen from "../screens/MessageScreen";
+import SuccessScreen from "../screens/SuccessScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function MainStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="DetailScreen" component={DetailScreen} />
+      <Stack.Screen name="MessageScreen" component={MessageScreen} />
+      <Stack.Screen name="SuccessScreen" component={SuccessScreen} />
+    </Stack.Navigator>
+  );
+}
